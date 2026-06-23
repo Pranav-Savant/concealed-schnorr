5c3b5305f0df0ddcfe361b4d19003dc9a78ef093
Pranav-Savant <pranavsavant1056@gmail.com>
2026-06-23 11:19:12 +0530

    Commit 4

--- examples/correctness.rs
+++ examples/correctness.rs
@@ -6,19 +6,23 @@ use concealed_schnorr::{
 
 use ark_bn254::{Fr, G1Projective as G1};
 use ark_ec::PrimeGroup;
-
+use  std::time::Instant;
 fn main() {
 
     let g = G1::generator();
     let m = Fr::from(911u64);
-    
-    let (sk, pk) =Schnorr::keyGen(g);
 
+    let start=Instant::now();
+    let (sk, pk) =Schnorr::keyGen(g);
+    println!("Key Generation Time:{:?}",start.elapsed());
+    
     let sig =Schnorr::Sign(g,m,&sk);
     
     println!("Signature Verify : {}",Schnorr::verify(g,&pk,&sig,m));
     
+    let start=Instant::now();
     let pcs = ParamCS::new();
+    println!("CS Setup Time:{:?}",start.elapsed());
     
     let (cs, t) =pcs.cs_convert(&sig,m,&pk);
     
